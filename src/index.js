984fc8936c0230277a78473c9c4e83b4960ade74
DimaMavrin <37770996+DimaMavrin@users.noreply.github.com>
2018-10-08 22:49:44 +0300

    new Solution

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  let count = base, a, answer = 0;
-  let i = 2
-  for (; i <= base; i++)
-    if ((count / i) === Math.round(count / i)) {
-      a = i
-      count = count / i
-      i--;
+  let tempBase = base;
+  let maxNumber = Number.MAX_VALUE;
+
+  for (let i = 2; i <= tempBase; i++) {
+    if (tempBase % i === 0) {
+      let denominator = 0;
+
+      while (tempBase % i === 0) {
+        tempBase /= i;
+        denominator++;
+      }
+
+      let innerRes = Math.floor(number / i);
+
+      let count = 0;
+      while (innerRes > 0) {
+        count += innerRes;
+        innerRes = Math.floor(innerRes / i);
+      }
+
+      maxNumber = Math.min(maxNumber, Math.floor(count / denominator));
     }
-  count = 1;
-  while (count <= number) {
-    count = count * a
-    answer = answer + Math.floor(number / count);
   }
-  return (answer);
+  return maxNumber; 
 
 }
